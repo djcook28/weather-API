aca19f67978a6baebd0019bca6f01e16661e0240
dcook <daniel.j.cook.28@gmail.com>
2025-05-24 22:00:26 -0500

    added functionality to provide all values for a specific station+year or a specific station

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from flask import Flask, render_template
-from weatherdf import get_station_data
+import weatherdf
+import pandas as pd
 
 app = Flask(__name__)
 
+weather_stations = pd.read_csv('data_small/stations.txt', skiprows=17)
+weather_stations = weather_stations[['STAID','STANAME                                 ']]
+
 @app.route('/')
 def home():
-    return render_template('home.html')
-
+    return render_template('home.html', data=weather_stations.to_html())
 
 @app.route('/api/v1/<station>/<date>')
 def about(station, date):
+    if(len(date) == 8):
+        temp = weatherdf.get_station_data(station, date)
+        return {'station': station,
+              'date': date,
+              'temperature': temp}
+    return weatherdf.get_year_data(station, date)
 
-    temp = get_station_data(station.zfill(3), date)
-    return {'station': station,
-          'date': date,
-          'temperature': temp}
+@app.route('/api/v1/<station>')
+def all_data(station):
+    return weatherdf.get_all_data(station)
 
 
 #__name__ only equals __main__ when this .py is run directly.
--- weatherdf.py
+++ weatherdf.py
@@ -2,22 +2,41 @@ import pandas as pd
 import numpy as np
 import datetime
 
-def clean_parameters(station_number, date):
+def clean_station(station_number):
     # zfill adds extra 0's to the string to make it the appropriate number of digits)
-    station_number = str(station_number).zfill(6)
-    date = date[:4] + '-' + date[4:6] + '-' + date[6:]
-    return station_number, date
+    return str(station_number).zfill(6)
+
+def clean_date(date):
+    return date[:4] + '-' + date[4:6] + '-' + date[6:]
 
 def get_station_data(station_number, date):
 
-    station_number, date = clean_parameters(station_number, date)
-
+    station_number = clean_station(station_number)
+    date = clean_date(date)
     df = pd.read_csv(f'data_small/TG_STAID{station_number}.txt', parse_dates=['    DATE'], skiprows=20)
-
     #when data is unavailable temperature is defaulted to -9999, additionally temperatures are in deci centigrade so
     # masking all place holder values and dividing all values by 10
     df['TG'] = (df['   TG'].mask(df[' Q_TG']==9, np.nan)/10)
-
     df['Fahrenheit'] = (df['TG'] * (9/5)) + 32
-
     return df.loc[df['    DATE'] == date]['Fahrenheit'].squeeze()
+
+def get_year_data(station_number, year):
+    station_number = clean_station(station_number)
+    df = pd.read_csv(f'data_small/TG_STAID{station_number}.txt', parse_dates=['    DATE'], skiprows=20)
+    #when data is unavailable temperature is defaulted to -9999, additionally temperatures are in deci centigrade so
+    # masking all place holder values and dividing all values by 10
+    df['TG'] = (df['   TG'].mask(df[' Q_TG']==9, np.nan)/10)
+    df['Fahrenheit'] = (df['TG'] * (9/5)) + 32
+    df['Year'] = pd.to_datetime(df['    DATE']).dt.year
+    df = df[df['Year'] == int(year)]
+    return df[['STAID','    DATE','Fahrenheit']].to_dict(orient='records')
+
+def get_all_data(station_number):
+
+    station_number = clean_station(station_number)
+    df = pd.read_csv(f'data_small/TG_STAID{station_number}.txt', parse_dates=['    DATE'], skiprows=20)
+    #when data is unavailable temperature is defaulted to -9999, additionally temperatures are in deci centigrade so
+    # masking all place holder values and dividing all values by 10
+    df['TG'] = (df['   TG'].mask(df[' Q_TG']==9, np.nan)/10)
+    df['Fahrenheit'] = (df['TG'] * (9/5)) + 32
+    return df[['STAID','    DATE','Fahrenheit']].to_dict(orient='records')
